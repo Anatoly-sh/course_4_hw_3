f37c7ff8810447ed1836108bbb3fe8bafc4f6ea9
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-02-19 20:02:44 +0300

    До тестированияю Все ок. Проверить нулевой стек

--- course_4_hw_3/utils.py
+++ course_4_hw_3/utils.py
@@ -1,35 +1,29 @@
 
 
 class Node:
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
-
 class Stack:
-    def __init__(self):
-        self.top = None
+    def __init__(self, top=None):
+        self.top = top
 
     def push(self, data):
         """ Тут добавление элемента """
-        new_node = Node(data)
-        new_node.next = self.top
+        new_node = Node(data, self.top)
         self.top = new_node
 
+node = Node('init')
 
-n1 = Node(5, None)
-n2 = Node('a', n1)
-print(n1.data)
-print(n2.data)
-print(n1)
-print(n2.next_node)
+stack = Stack(node)
 
-stack = Stack()
 stack.push('data1')
 stack.push('data2')
 stack.push('data3')
+stack.push('data4')
+
 print(stack.top.data)
 print(stack.top.next_node.data)
 print(stack.top.next_node.next_node.data)
-print(stack.top.next_node.next_node.next_node)
 print(stack.top.next_node.next_node.next_node.data)
