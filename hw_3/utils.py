867fa5cc9d9029e7e86776838e0f50a06ac69228
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-02-21 21:25:14 +0300

    Тестирование - ОК. Добавлен README в commit

--- hw_3/utils.py
+++ hw_3/utils.py
@@ -17,6 +17,12 @@ class Stack:
         new_node = Node(data, self.top)
         self.top = new_node
 
+    def pop(self):
+        """Извлечение элемента"""
+        pop_adr = self.top
+        self.top = self.top.next_node
+        return pop_adr.data
+
 
 if __name__ == '__main__':
     node = Node(None)
@@ -26,15 +32,17 @@ if __name__ == '__main__':
     stack.push('data1')
     stack.push('data2')
     stack.push('data3')
+    print(stack.pop(), 'pop')
+    print(stack.pop(), 'pop')
 
     print(stack.top.data)
     print(stack.top.next_node.data)
-    print(stack.top.next_node.next_node.data)
-    print(stack.top.next_node.next_node.next_node.data)
+    # print(stack.top.next_node.next_node.data)
+    # print(stack.top.next_node.next_node.next_node.data)
 
-    n1 = Node(5, None)
-    n2 = Node('a', n1)
-    print(n1.data)
-    print(n2.data)
-    print(n1)
-    print(n2.next_node)
+    # n1 = Node(5, None)
+    # n2 = Node('a', n1)
+    # print(n1.data)
+    # print(n2.data)
+    # print(n1)
+    # print(n2.next_node)
