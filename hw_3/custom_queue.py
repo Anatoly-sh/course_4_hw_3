df37b0c899b3280c8897659d24b042bf0ddf862a
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-03-05 10:13:48 +0300

    Данные извлекаются. Разобраться с границами

--- hw_3/custom_queue.py
+++ hw_3/custom_queue.py
@@ -16,3 +16,28 @@ class Queue:
         else:
             self.tail.next_node = new_node
             self.tail = new_node
+
+    def dequeue(self):
+        """Извлекает данные из очереди"""
+        data = self.head.data
+        self.head = self.head.next_node
+
+        return data
+
+
+if __name__ == '__main__':
+    queue = Queue()
+    queue.enqueue('data1')
+    queue.enqueue('data2')
+    queue.enqueue('data3')
+
+    print(queue.head.data)
+    print(queue.head.next_node.data)
+    print(queue.tail.data)
+    print(queue.tail.next_node)
+    # print(queue.tail.next_node.data)
+
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
+    print(queue.dequeue())
