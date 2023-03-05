32076142f285e867fc316a8f571d1e5680fe8a05
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-03-05 10:32:31 +0300

    К сдачею Тесты - ОК.

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -21,3 +21,16 @@ class TestQueueEnqueue(unittest.TestCase):
         assert queue.head.next_node.data == 'data2'
         assert queue.tail.data == 'data3'
         assert queue.tail.next_node is None
+
+
+class TestQueueDequeue(unittest.TestCase):
+    """Проверка извлечения из очередь и ее границы"""
+    def test_queue_Dequeue(self):
+        queue = Queue()
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        assert queue.dequeue() == 'data1'
+        assert queue.dequeue() == 'data2'
+        assert queue.dequeue() == 'data3'
+        assert queue.dequeue() is None
