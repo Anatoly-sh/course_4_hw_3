e394e60fe579e83bdacc8697473cfb805402701a
Анатолий Шаповалов <anatoly.shapovalov@gmail.com>
2023-03-09 21:34:21 +0300

    Код готов. Проверить ожидаемое поведение

---
+++ hw_3/linked_list.py
@@ -0,0 +1,38 @@
+class Node:
+    """Класс узла с данными и адресом соседнего узла"""
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+
+class LinkedList:
+    """Класс связанного списка"""
+
+    def __init__(self, start_point=None, end_point = None):
+        self.start_point = start_point
+        self.end_point = end_point
+
+    def insert_beginning(self, data):
+        """ Тут добавление элемента в начале"""
+        new_node = Node(data, self.start_point)
+        new_node.nextnode = self.start_point
+        self.start_point = new_node
+
+    def insert_at_end(self, data):
+        """Тут добавление элемента в конце"""
+        new_node = Node(data)
+        if self.end_point is not None:
+            self.end_point.next_node = new_node
+            self.end_point = new_node
+
+
+ll = LinkedList()
+
+
+ll.insert_beginning({'id': 1})
+ll.insert_at_end({'id': 2})
+ll.insert_at_end({'id': 3})
+ll.insert_beginning({'id': 0})
+#ll.print_ll(){'id': 0}-> {'id': 1} -> {'id': 2} -> {'id': 3} -> None
+print(ll.end_point)
+print(ll.start_point.data)
